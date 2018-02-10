26d9b0446e960ede915a658c1bb0e226fbdff475
Alphatiq <aurelien.potot@icloud.com>
2018-02-10 18:19:22 +0100

    Add files via upload

---
+++ commands/play.js
@@ -0,0 +1,31 @@
+const Command = require('./command')
+const YoutubeStream = require('ytdl-core')
+
+module.exports = class Play extends Command {
+    
+    static match (message) {
+        return message.content.startsWith('.play')
+    }
+    
+    static action (message) {
+    let voiceChannel = message.guild.channels
+        .filter(function (channel) { return channel.type === 'voice'})
+        .first()
+    let args = message.content.split(' ')
+    voiceChannel
+        .join()
+        .then(function (connection){
+        let stream = YoutubeStream(args[1])
+        stream.on('error', function(){
+            message.channel.send("Je ne peux pas lire cette vidéo :(")
+            connection.disconnect()
+        })
+        connection
+            .playStream(stream)
+            .on('end', function () {
+            connection.disconnect()
+        })
+    })
+}
+
+}
